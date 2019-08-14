c9829eded1a5be2b409cb0a8498f6579b6f25ae3
bab2683 <barahona.braulio@gmail.com>
2019-08-14 15:25:42 +0100

    descructuring environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
+const {
+  firebase: {
+    FIREBASE_MAPS_API_KEY,
+    FIREBASE_MAPS_AUTH_DOMAIN,
+    FIREBASE_MAPS_DATABASE_URL,
+    FIREBASE_MAPS_PROJECT_ID,
+    FIREBASE_MAPS_STORAGE_BUCKET,
+    FIREBASE_MAPS_MESSAGING_ID
+  }
+} = environment;
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [AngularFireModule.initializeApp(environment.firebase), AppRoutingModule, BrowserModule],
+  imports: [
+    AngularFireModule.initializeApp({
+      apiKey: FIREBASE_MAPS_API_KEY,
+      authDomain: FIREBASE_MAPS_AUTH_DOMAIN,
+      databaseURL: FIREBASE_MAPS_DATABASE_URL,
+      projectId: FIREBASE_MAPS_PROJECT_ID,
+      storageBucket: FIREBASE_MAPS_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_MAPS_MESSAGING_ID
+    }),
+    AppRoutingModule,
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
